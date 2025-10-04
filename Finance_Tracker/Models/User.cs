c0a4604e0912ce9a2d4040a28ff8e42fb92664c9
Braun Roland <rolibraun94@gmail.com>
2025-10-04 18:40:25 +0200

    User Modell létrehozva

---
+++ Finance_Tracker/Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace Finance_Tracker.Models
+{
+    public class User
+    {
+        private int id;
+        private string username;
+        private string passwordHash;
+        private string email;
+        private DateTime createdAt;
+
+
+        public User(int id, string username, string passwordHash, string email, DateTime createdAt)
+        {
+            this.id = id;
+            this.username = username;
+            this.passwordHash = passwordHash;
+            this.email = email;
+            this.createdAt = createdAt;
+        }
+        public int Id { get => id; set => id = value; }
+        public string Username { get => username; set => username = value; }
+        public string PasswordHash { get => passwordHash; set => passwordHash = value; }
+        public string Email { get => email; set => email = value; }
+        public DateTime CreatedAt { get => createdAt; set => createdAt = value; }
+    }
+}
