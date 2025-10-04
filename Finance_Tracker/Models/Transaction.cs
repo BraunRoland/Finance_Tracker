1eccf67462531f11ca4867e277bb5d11f3dee629
Braun Roland <rolibraun94@gmail.com>
2025-10-04 18:52:20 +0200

    Transaction modell létrehozva

---
+++ Finance_Tracker/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace Finance_Tracker.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; } // "income" or "expense"
+
+        public Transaction()
+        {
+            
+        }
+    }
+}
