37382e0a60482dfa392c0c2bdc2700520aadfaa0
Braun Roland <rolibraun94@gmail.com>
2025-10-04 18:54:05 +0200

    Category modell létrehozva

---
+++ Finance_Tracker/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace Finance_Tracker.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int USerId { get; set; }
+        public string Name { get; set; }
+
+        public Category()
+        {
+            
+        }
+    }
+}
